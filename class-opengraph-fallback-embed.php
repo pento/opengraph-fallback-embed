14a70a9f901e44b2d9c5a8c1dbfc53a1f0119242
Gary Pendergast <gary@pento.net>
2026-03-26 10:55:10 +1100

    chore: bump version to 1.3.7.

--- class-opengraph-fallback-embed.php
+++ class-opengraph-fallback-embed.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: OpenGraph Fallback Embed
  * Description: Provides an embed block based on a site's OpenGraph tags when no other embed handler matches the URL.
- * Version: 1.3.6
+ * Version: 1.3.7
  * Author: pento
  * Author URI: https://pento.net
  * License: GPL-2.0-or-later
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: https://github.com/sponsors/pento
 Tags: embed, opengraph, link, card, block
 Requires at least: 6.3
 Tested up to: 7.0
-Stable tag: 1.3.6
+Stable tag: 1.3.7
 Requires PHP: 7.4
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
